d5a0d9bfd93088bfef6781813caf5ddf56beb6ad
Rudera <49738576+rudera@users.noreply.github.com>
2021-03-17 15:38:26 -0400

    Fixed API usage and worked on preprocessing

--- app.py
+++ app.py
@@ -4,32 +4,33 @@ from time import sleep
 from datetime import timezone
 from dateutil.rrule import HOURLY, rrule
 from dateutil.parser import parse
-import pandas as pandas
+import pandas as pd
 from algosdk.v2client import indexer
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-indexerToken = os.environ.get('PURESTAKE_API_KEY')
-myindexer = indexer.IndexerClient(indexer_token=indexerToken, indexer_address='https://testnet-algorand.api.purestake.io/idx2')
+algod_address = "https://testnet-algorand.api.purestake.io/idx2"
+algod_token = ""
+headers = {
+	"X-API-Key": os.environ.get('PURESTAKE_API_KEY')
+}
 
-def get_txn_response(start_time, end_time, asset_id):
-	"""
-	Returns all transactions between start_time and end_time for any asset_id
-	"""
-	start_time = start_time.astimezone(timezone.utc).isoformat('T')
-	end_time = end_time.astimezone(timezone.utc).isoformat('T')
+myindexer = indexer.IndexerClient(algod_token, algod_address, headers)
 
-	nexttoken = ''
-	numtx = 1
 
-	responses = []
+def get_asset_txn(asset_id):
+	 nexttoken = ""
+	 numtx = 1
+	 responses = []
 
-	while numtx > 0:
-		response = myindexer.search_transactions(start_time=start_time, end_time=end_time, asset_id=asset_id, limit=1000)
-		transactions = response['transactions']
-		responses += transactions
-		numtx = len(transactions)
-		if numtx > 0:
-			nexttoken = response['next-token']
-	return responses
+	 while numtx > 0:
+	 	response = myindexer.search_asset_transactions(asset_id = asset_id, next_page = nexttoken, limit-1000)
+	 	transactions = response['transactions']
+	 	responses += transactions
+	 	numtx = len(transactions)
+	 	if numtx > 0:
+	 		nexttoken = response['next-token']
+	 return responses
+
+
